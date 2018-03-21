777e0599b99c8815e98a84406cb7e8d4bedc02df
Paweł Jadanowski <pjadanowski@gmail.com>
2018-03-21 21:26:27 +0100

    stubs

--- Shopping/Product.cs
+++ Shopping/Product.cs
@@ -6,11 +6,13 @@ using System.Threading.Tasks;
 
 namespace Shopping
 {
-    public class Product
+    public class Product : IProduct
     {
         public string Name { get; set; }
         public double Price { get; set; }
         public int Quantity { get; set; }
+
+        private IProduct _Product;
 
 
         public Product(string name, double price, int quantity)
@@ -18,6 +20,25 @@ namespace Shopping
             this.Name = name;
             this.Price = price;
             this.Quantity = quantity;
+        }
+
+
+        public Product(IProduct iProduct)
+        {
+            _Product = iProduct;
+        }
+
+
+
+        public double GetProductPrice(string productName)
+        {
+            double price = _Product.GetProductPriceFromDB(productName);
+            return price;
+        }
+
+        public double GetProductPriceFromDB(string productName)
+        {
+            return Price;
         }
     }
 }
---
+++ ShoppingTest/StubTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Net.Mime;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Fakes;
+using Shopping;
+
+namespace ShoppingTest
+{
+    /// <summary>
+    /// Opis podsumowujący elementu StubTest
+    /// </summary>
+    [TestClass]
+    public class StubTest
+    {
+        public StubTest()
+        {
+        }
+
+        private TestContext testContextInstance;
+
+      
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Dodatkowe atrybuty testu
+        //
+        // Można użyć następujących dodatkowych atrybutów w trakcie pisania testów:
+        //
+        // Użyj ClassInitialize do uruchomienia kodu przed uruchomieniem pierwszego testu w klasie
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Użyj ClassCleanup do uruchomienia kodu po wykonaniu wszystkich testów w klasie
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Użyj TestInitialize do uruchomienia kodu przed uruchomieniem każdego testu 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Użyj TestCleanup do uruchomienia kodu po wykonaniu każdego testu
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+//            IProduct iproduct = new Application.Business.Fakes.StubI
+            Product product = new Product(iProduct);
+            double result = product.GetProductPrice("Koszula");
+            Assert.AreEqual(49.99, result);
+        }
+    }
+}
